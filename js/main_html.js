8da75d24265e01c5b2fa5e9ae5f033415d0de664
ju4np1g4llo <juanpigallo01@gmail.com>
2022-10-24 19:46:57 -0300

    calendario

--- js/main_html.js
+++ js/main_html.js
@@ -6,28 +6,6 @@ var appVue1 = new Vue ({
   }
 })
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 var words = document.getElementsByClassName('word');
 var wordArray = [];
 var currentWord = 0;
@@ -55,7 +33,7 @@ function changeWord() {
 
 function animateLetterOut(cw, i) {
   setTimeout(function() {
-		cw[i].className = 'letter out';
+    cw[i].className = 'letter out';
   }, i*80);
 }
 
@@ -82,7 +60,6 @@ function splitLetters(word) {
 
 changeWord();
 setInterval(changeWord, 4000);
-
 
 
 
