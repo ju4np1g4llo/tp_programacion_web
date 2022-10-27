e33a2775de6149b75e27e6e20943259811368569
Nicolas Peiretti <nico.peiretti157@gmail.com>
2022-10-27 09:17:17 -0300

    Cambios

---
+++ src/Formulario.vue
@@ -0,0 +1,18 @@
+<template>
+  <header-top></header-top>
+  <Form></Form>
+</template>
+
+<script>
+import HeaderTop from "@/App";
+import Form from "@/components/Form";
+
+
+export default {
+  name: 'Formulario',
+  components: {
+    Form,
+    HeaderTop,
+  }
+}
+</script>
---
+++ src/components/Form.vue
@@ -0,0 +1,78 @@
+<template>
+  <form
+      action="mailto:peinchaust@ucema.edu.ar" method="post" enctype="text/plain">
+
+
+    <fieldset>
+      <legend>IDENTIFICACION PERSONAL</legend>
+
+
+      <table border="0">
+        <tbody>
+
+        <tr>
+          <td>Nombre y Apellido</td>
+          <td>
+            <input type="text" name="nombre" id=""
+                   size="30"
+                   placeholder="Ingrese el Nombre y Apellido">
+          </td>
+        </tr>
+        <tr>
+          <td>Edad</td>
+          <td>
+            <input type="number" name="edad" id="" value="18"
+                   step="0.5">
+          </td>
+        </tr>
+        <tr>
+          <td>Sexo</td>
+          <td>
+            <input type="radio" name="sexo" id="" value="M">Masc.
+            <input type="radio" name="sexo" id="" value="F">Fem.
+            <input type="radio" name="sexo" id="" value="O">Otro
+            <input type="radio" name="sexo" id="" value="PNO">Pref. no decir
+
+
+          </td>
+        </tr>
+        <tr>
+          <td>Teléfono</td>
+          <td>
+            <select name="prefijo" id="">
+              <option value="+51">Perú</option>
+              <option value="+549" selected>Argentina</option>
+              <option value="+55">Brasil</option>
+              <option value="+56">Chile</option>
+              <option value="+595">Paraguay</option>
+              <option value="+598">Uruguay</option>
+            </select>
+            <input type="text" name="" id=""
+                   maxlength="10">
+          </td>
+        </tr>
+        </tbody>
+      </table>
+      <label for="mailCV">Mail: </label>
+      <input type="email" name="mailCV" id="" value="@">
+    </fieldset>
+
+    <br>
+
+
+    <br><br>
+    <input type="submit" value="Enviar">
+    <input type="reset" value="Borrar Formulario">
+
+  </form>
+</template>
+
+<script>
+export default {
+  name: "Form"
+}
+</script>
+
+<style scoped>
+
+</style>
