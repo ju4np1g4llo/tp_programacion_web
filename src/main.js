8738a2023a4a41f2b6cd84f5e8221d6dd94855d2
ju4np1g4llo <juanpigallo01@gmail.com>
2022-11-05 16:03:30 -0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -34,7 +34,7 @@ import axios from "axios";
 
 
 function getPurchaseOrders() {
-    axios.get('http://127.0.0.1:5000/api/v1/purchase-orders/')
+    axios.get('http://127.0.0.1:5000/api/v1/lista-pacientes/')
         .then(function (response) {
             for (let i=0 ; i<response.data.length ; i++) {
                 addTableDataRow(response.data[i]);
